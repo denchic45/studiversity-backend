3f26592cdea4b53774a3d3d2bf0611d31313c591
Denis <denchic150@gmail.com>
2022-11-17 19:26:59 +0300

    Update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,10 @@ plugins {
     kotlin("jvm") version "1.7.21"
     id("io.ktor.plugin") version "2.1.3"
                 id("org.jetbrains.kotlin.plugin.serialization") version "1.7.21"
+
+    // Shadow plugin - enable support for building our UberJar
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+    war
 }
 
 group = "com.studiversity"
@@ -20,6 +24,40 @@ application {
 
 repositories {
     mavenCentral()
+}
+
+tasks {
+    val shadowJarTask = named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar") {
+        // explicitly configure the filename of the resulting UberJar
+        val uberJarFileName =
+            "com.denchic45.studiversity-backend-0.0.1.jar"
+//            "${project.name}-${project.version}-all.jar\"" //todo переиспользовать!
+        archiveFileName.set(uberJarFileName)
+
+        // Appends entries in META-INF/services resources into a single resource. For example, if there are several
+        // META-INF/services/org.apache.maven.project.ProjectBuilder resources spread across many JARs the individual
+        // entries will all be concatenated into a single META-INF/services/org.apache.maven.project.ProjectBuilder
+        // resource packaged into the resultant JAR produced by the shading process -
+        // Effectively ensures we bring along all the necessary bits from Jetty
+        mergeServiceFiles()
+
+        // As per the App Engine java11 standard environment requirements listed here:
+        // https://cloud.google.com/appengine/docs/standard/java11/runtime
+        // Your Jar must contain a Main-Class entry in its META-INF/MANIFEST.MF metadata file
+        manifest {
+            attributes(mapOf("Main-Class" to application.mainClass.get()))
+        }
+    }
+
+    // because we're using shadowJar, this task has limited value
+    named("jar") {
+        enabled = false
+    }
+
+    // update the `assemble` task to ensure the creation of a brand new UberJar using the shadowJar task
+    named("assemble") {
+        dependsOn(shadowJarTask)
+    }
 }
 
 dependencies {
