dcca76126479c109d18df4b51f66a6d20aac5e0c
Denis <denchic150@gmail.com>
2022-11-20 00:23:05 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ plugins {
 group = "com.studiversity"
 version = "0.0.1"
 application {
-    mainClass.set("com.studiversity.ApplicationKt")
+    mainClass.set("io.ktor.server.netty.EngineMain")
 
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
