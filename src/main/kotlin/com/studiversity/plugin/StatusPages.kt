51866af1ec2487c4dbaadcd44e3e3a2af1a2cbcd
Denis <denchic150@gmail.com>
2022-12-11 23:25:15 +0300

    Move status page configuration to separate file

---
+++ src/main/kotlin/com/studiversity/plugin/StatusPages.kt
@@ -0,0 +1,36 @@
+package com.studiversity.plugin
+
+import com.studiversity.feature.role.ForbiddenException
+import com.studiversity.model.ErrorInfo
+import com.studiversity.model.respondWithError
+import com.studiversity.model.respondWithErrors
+import com.studiversity.model.toErrors
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.*
+import io.ktor.server.plugins.requestvalidation.*
+import io.ktor.server.plugins.statuspages.*
+
+fun Application.configureStatusPages() {
+    install(StatusPages) {
+        exception<BadRequestException> { call, exception ->
+            call.respondWithError(
+                HttpStatusCode.BadRequest,
+                ErrorInfo(
+                    if (exception.message == "Illegal input") {
+                        "INVALID_INPUT"
+                    } else exception.message ?: ""
+                )
+            )
+        }
+        exception<NotFoundException> { call, exception ->
+            call.respondWithError(HttpStatusCode.NotFound, ErrorInfo(exception.message ?: ""))
+        }
+        exception<ForbiddenException> { call, exception ->
+            call.respondWithError(HttpStatusCode.Forbidden, ErrorInfo(exception.message ?: ""))
+        }
+        exception<RequestValidationException> { call, cause ->
+            call.respondWithErrors(HttpStatusCode.BadRequest, cause.reasons.toErrors())
+        }
+    }
+}
