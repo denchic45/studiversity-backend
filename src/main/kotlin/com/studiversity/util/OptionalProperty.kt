3176b3224550457f927dd48c6c6813f9e6e3fe9b
Denis <denchic150@gmail.com>
2022-12-11 23:17:48 +0300

    Add wrapper for optional values

--- src/main/kotlin/com/studiversity/util/OptionalProperty.kt
+++ src/main/kotlin/com/studiversity/util/OptionalProperty.kt
@@ -5,4 +5,8 @@ sealed class OptionalProperty<out T> {
     object NotPresent : OptionalProperty<Nothing>()
 
     data class Present<T>(val value: T) : OptionalProperty<T>()
+
+    fun ifPresent(onPresent: (value: T) -> Unit) {
+        if (this is Present) onPresent(value)
+    }
 }
