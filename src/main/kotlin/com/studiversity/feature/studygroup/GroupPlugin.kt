c0f1a7405e544c4d17e91c19fc1a14d7b2422d02
Denis <denchic150@gmail.com>
2022-12-12 11:53:01 +0300

    Rename object

--- src/main/kotlin/com/studiversity/feature/studygroup/GroupPlugin.kt
+++ src/main/kotlin/com/studiversity/feature/studygroup/GroupPlugin.kt
@@ -8,8 +8,9 @@ fun Application.groupsModule() {
     groupRoutes()
 }
 
-object GroupErrors {
+object StudyGroupErrors {
     const val INVALID_GROUP_NAME = "INVALID_GROUP_NAME"
     const val INVALID_ACADEMIC_YEAR = "INVALID_ACADEMIC_YEAR"
     const val GROUP_DOES_NOT_EXIST = "GROUP_DOES_NOT_EXIST"
+    const val MEMBER_ALREADY_ENROLLED_IN_GROUP = "MEMBER_ALREADY_ENROLLED_IN_GROUP"
 }
