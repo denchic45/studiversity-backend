ab9cc3354619f66775cc4884d3797fd1153bdc19
Denis <denchic150@gmail.com>
2022-12-12 11:55:37 +0300

    Update add func

--- src/main/kotlin/com/studiversity/feature/studygroup/repository/StudyGroupMemberRepository.kt
+++ src/main/kotlin/com/studiversity/feature/studygroup/repository/StudyGroupMemberRepository.kt
@@ -1,21 +1,20 @@
 package com.studiversity.feature.studygroup.repository
 
-import com.studiversity.database.table.RoleDao
 import com.studiversity.database.table.UsersRolesScopes
-import com.studiversity.feature.studygroup.model.EnrolStudyGroupMemberRequest
+import com.studiversity.feature.role.Role
 import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.transactions.transaction
 import java.util.*
 
 class StudyGroupMemberRepository {
 
-    fun add(groupId: UUID, enrolStudyGroupMemberRequest: EnrolStudyGroupMemberRequest) = transaction {
-        enrolStudyGroupMemberRequest.roles.forEach { role ->
+    fun add(groupId: UUID, userId: UUID, roles: List<Role>) = transaction {
+        roles.map { role ->
             UsersRolesScopes.insert {
-                it[userId] = UUID.fromString(enrolStudyGroupMemberRequest.userId)
-                it[roleId] = RoleDao.findIdByName(role)
+                it[UsersRolesScopes.userId] = userId
+                it[roleId] = role.id
                 it[scopeId] = groupId
-            }
-        }
+            }.run { insertedCount > 0 }
+        }.all { it }
     }
 }
