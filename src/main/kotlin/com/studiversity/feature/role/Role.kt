5ad5aca9c9847891c7b0895af37cf177390100d6
Denis <denchic150@gmail.com>
2022-12-12 11:54:06 +0300

    Add id property

--- src/main/kotlin/com/studiversity/feature/role/Role.kt
+++ src/main/kotlin/com/studiversity/feature/role/Role.kt
@@ -1,16 +1,16 @@
 package com.studiversity.feature.role
 
-data class Role(val resource: String) {
+data class Role(val id: Long, val resource: String) {
 
     override fun toString(): String = resource
 
     companion object {
-        val Guest: Role = Role("guest")
-        val User: Role = Role("user")
-        val Student: Role = Role("student")
-        val Teacher: Role = Role("teacher")
-        val Moderator: Role = Role("moderator")
-        val Headman: Role = Role("headman")
-        val Curator: Role = Role("curator")
+        val Guest: Role = Role(1, "guest")
+        val User: Role = Role(2, "user")
+        val Student: Role = Role(3, "student")
+        val Teacher: Role = Role(4, "teacher")
+        val Moderator: Role = Role(5, "moderator")
+        val Headman: Role = Role(6, "headman")
+        val Curator: Role = Role(7, "curator")
     }
 }
