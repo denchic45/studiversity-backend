9b599211e6dcdadd98281d90addcc63d9eed0f29
Denis <denchic150@gmail.com>
2022-12-12 11:53:40 +0300

    Rename object

--- src/main/kotlin/com/studiversity/feature/role/GroupErrors.kt
+++
@@ -1,5 +0,0 @@
-package com.studiversity.feature.role
-
-object RoleErrors {
-    const val NOT_ALLOWED_ROLE_IN_SCOPE = "NOT_ALLOWED_ROLE_IN_SCOPE"
-}
---
+++ src/main/kotlin/com/studiversity/feature/role/RoleErrors.kt
@@ -0,0 +1,8 @@
+package com.studiversity.feature.role
+
+object RoleErrors {
+    const val PERMISSION_DENIED = "PERMISSION_DENIED"
+    const val NOT_AVAILABLE_ROLE_IN_SCOPE = "NOT_AVAILABLE_ROLE_IN_SCOPE"
+    const val PERMISSION_DENIED_TO_ASSIGN_ROLE = "NOT_PERMISSION_ASSIGN_ROLE"
+    const val NOT_FOUND_ROLE = "NOT_FOUND_ROLE"
+}
