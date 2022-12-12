3e15f4f6c1190002262a428589316af540c57ed0
Denis <denchic150@gmail.com>
2022-12-12 11:55:47 +0300

    Rename entity to dao

--- src/main/kotlin/com/studiversity/feature/scope/AddScopeRepositoryExt.kt
+++ src/main/kotlin/com/studiversity/feature/scope/AddScopeRepositoryExt.kt
@@ -1,20 +1,20 @@
 package com.studiversity.feature.scope
 
-import com.studiversity.database.table.ScopeEntity
-import com.studiversity.database.table.ScopeTypeEntity
+import com.studiversity.database.table.ScopeDao
+import com.studiversity.database.table.ScopeTypeDao
 import java.util.*
 
 interface AddScopeRepoExt {
     fun addScope(scopeId: UUID, scopeTypeId: Long, parentScopeId: UUID) {
-        val parentScope = ScopeEntity.findById(parentScopeId)!!
+        val parentScope = ScopeDao.findById(parentScopeId)!!
         val parentType = parentScope.type
-        val childScopeType = ScopeTypeEntity.findById(scopeTypeId)!!
+        val childScopeType = ScopeTypeDao.findById(scopeTypeId)!!
         if (childScopeType.parentId.value != parentType.id.value)
             throw IllegalArgumentException(
                 "type of passed parentScopeId $parentScope is not parent type of passed scopeTypeId $scopeTypeId"
             )
-        ScopeEntity.new(scopeId) {
-            type = ScopeTypeEntity.findById(scopeTypeId)!!
+        ScopeDao.new(scopeId) {
+            type = ScopeTypeDao.findById(scopeTypeId)!!
             path = listOf(scopeId) + parentScope.path
         }
     }
