3d8d25e6d22440a3eefaec58051a011f186fb2a5
Denis <denchic150@gmail.com>
2022-12-13 22:29:49 +0300

    add map to UserRolesResponse

--- src/main/kotlin/com/studiversity/feature/role/mapper/RoleMapper.kt
+++ src/main/kotlin/com/studiversity/feature/role/mapper/RoleMapper.kt
@@ -1,8 +1,14 @@
 package com.studiversity.feature.role.mapper
 
 import com.studiversity.database.table.RoleDao
+import com.studiversity.database.table.UserRoleScopeDao
 import com.studiversity.feature.role.Role
+import com.studiversity.feature.role.model.UserRolesResponse
+import java.util.*
 
 fun RoleDao.toRole(): Role = Role(id = id.value, resource = shortName)
 
-fun Iterable<RoleDao>.toRoles(): List<Role> = map(RoleDao::toRole)
+fun Iterable<UserRoleScopeDao>.toUserRolesResponse(userId: UUID): UserRolesResponse = UserRolesResponse(
+    userId = userId,
+    roles = map { it.role.toRole() }
+)
