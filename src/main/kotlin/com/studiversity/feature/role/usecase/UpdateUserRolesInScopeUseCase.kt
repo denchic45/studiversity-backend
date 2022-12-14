86653854de2694a52a4594f5215bcbdde1901459
Denis <denchic150@gmail.com>
2022-12-14 11:39:05 +0300

    Rename invoke fun

--- src/main/kotlin/com/studiversity/feature/role/usecase/UpdateUserRolesInScopeUseCase.kt
+++ src/main/kotlin/com/studiversity/feature/role/usecase/UpdateUserRolesInScopeUseCase.kt
@@ -10,7 +10,7 @@ import java.util.*
 class UpdateUserRolesInScopeUseCase(
     private val roleRepository: RoleRepository
 ) {
-    fun updateUserRolesInScope(
+    operator fun invoke(
         userId: UUID,
         scopeId: UUID,
         updateUserRolesRequest: UpdateUserRolesRequest
