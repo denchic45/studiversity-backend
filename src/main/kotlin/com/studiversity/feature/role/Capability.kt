7cb83858b79dfb18476c14b68b78f7601aa286c3
Denis <denchic150@gmail.com>
2023-01-03 23:04:09 +0300

    Rename study group capabilities

--- src/main/kotlin/com/studiversity/feature/role/Capability.kt
+++ src/main/kotlin/com/studiversity/feature/role/Capability.kt
@@ -11,16 +11,17 @@ data class Capability(val resource: String) {
 
         val WriteAssignRoles: Capability = Capability("role/assignment:write")
 
-        val WriteGroup: Capability = Capability("group:write")
-        val ReadGroup: Capability = Capability("group:read")
-        val DeleteGroup: Capability = Capability("group:delete")
-        val WriteGroupMembers: Capability = Capability("group/members:write")
+        val WriteStudyGroup: Capability = Capability("study_group:write")
+        val ReadStudyGroup: Capability = Capability("study_group:read")
+        val DeleteStudyGroup: Capability = Capability("study_group:delete")
+        val WriteStudyGroupMembers: Capability = Capability("study_group/members:write")
 
         val WriteCourses: Capability = Capability("course:write")
         val ReadCourse: Capability = Capability("course:read")
         val DeleteCourse: Capability = Capability("course:delete")
         val WriteCourseMembers: Capability = Capability("course/members:write")
         val ReadCourseMembers: Capability = Capability("course/members:read")
+        val WriteCourseStudyGroups: Capability = Capability("course/study_group:write")
 
         val WriteSubject: Capability = Capability("subject:write")
         val ReadSubject: Capability = Capability("subject:read")
