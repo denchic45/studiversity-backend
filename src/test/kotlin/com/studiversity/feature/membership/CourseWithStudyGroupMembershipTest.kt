14e27e6b9c91a68650a13caeef98ce8f5cf0e499
Denis <denchic150@gmail.com>
2023-01-05 13:19:30 +0300

    Add a second test and structure the test

--- src/test/kotlin/com/studiversity/feature/membership/CourseWithStudyGroupMembershipTest.kt
+++ src/test/kotlin/com/studiversity/feature/membership/CourseWithStudyGroupMembershipTest.kt
@@ -1,5 +1,6 @@
 package com.studiversity.feature.membership
 
+import com.studiversity.feature.auth.model.LoginRequest
 import com.studiversity.feature.course.model.CourseResponse
 import com.studiversity.feature.course.model.CreateCourseRequest
 import com.studiversity.feature.membership.model.ManualJoinMemberRequest
@@ -11,60 +12,183 @@ import com.studiversity.supabase.model.SignupResponse
 import com.studiversity.util.toUUID
 import io.ktor.client.*
 import io.ktor.client.call.*
+import io.ktor.client.plugins.auth.*
+import io.ktor.client.plugins.auth.providers.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.testing.*
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.json.Json
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.*
+import java.util.*
 import kotlin.test.assertEquals
 
 class CourseWithStudyGroupMembershipTest {
 
-    var token = ""
-    var refreshToken = ""
+    companion object {
+        private lateinit var testApp: TestApplication
+        lateinit var client: HttpClient
+        lateinit var startData: StartData
 
-//    private var startData: StartData by Delegates.notNull()
-
-    @Test
-    fun testMembersOnAttachDetachStudyGroupsToCourse() = testApplication {
-        val client = createClient {
-            install(ContentNegotiation) {
-                json(Json {
-                    prettyPrint = true
-                    isLenient = true
-                    ignoreUnknownKeys = true
-                    encodeDefaults = true
-                })
+        @JvmStatic
+        @BeforeAll
+        fun setup(): Unit = runBlocking {
+            testApp = TestApplication {}
+            client = testApp.createClient {
+                install(ContentNegotiation) {
+                    json(Json {
+                        prettyPrint = true
+                        isLenient = true
+                        ignoreUnknownKeys = true
+                        encodeDefaults = true
+                    })
+                }
+                install(Auth) {
+                    bearer {
+                        refreshTokens {
+                            BearerTokens(accessToken = client.post("/auth/token?grant_type=password") {
+                                contentType(ContentType.Application.Json)
+                                setBody(LoginRequest("denchic150@gmail.com", "OBDIhi76534g33"))
+                            }.body<SignupResponse>().accessToken, "")
+                        }
+                    }
+                }
             }
-//            install(Auth) {
-//                basic {  }
-//                bearer {
-//                    loadTokens {
-//                        // Load tokens from a local storage and return them as the 'BearerTokens' instance
-//                        BearerTokens(token,refreshToken)
-//                    }
-//
-//                    refreshTokens { this.client. }
-//                }
-//            }
         }
 
-        token = client.post("/auth/token?grant_type=password") {
-            setBody(
-                """
-                {
-                  "email": "denchic150@gmail.com",
-                  "password": "OBDIhi76534g33"
-                }
-            """.trimIndent()
-            )
-        }.body<SignupResponse>().accessToken
+        @JvmStatic
+        @AfterAll
+        fun teardown() = runBlocking {
+            testApp.stop()
+        }
+    }
 
+    @Test
+    fun testMembersOnAttachDetachStudyGroupsToCourse(): Unit = runBlocking {
+        val (studyGroup1: StudyGroupResponse, studyGroup2: StudyGroupResponse, course: CourseResponse) = startData
+
+        attachGroupsToCourse(client, course, studyGroup1, studyGroup2)
+
+        client.get("/courses/${course.id}/studygroups")
+            .body<List<String>>().apply {
+                assertEquals(map(String::toUUID).toSet(), setOf(studyGroup1.id, studyGroup2.id))
+            }
+
+        val user1Id = "7a98cdcf-d404-4556-96bd-4ce9137c8cbe".toUUID()
+        val user2Id = "77129e28-bf01-4dca-b19f-9fbcf576345e".toUUID()
+
+        enrolStudentsToGroups(client, studyGroup1, studyGroup2, user2Id, user1Id)
+
+        client.get("/courses/${course.id}/members").body<List<ScopeMember>>()
+            .apply { assertEquals(setOf(user1Id, user2Id), map { it.userId }.toSet()) }
+
+        // delete first group and check members of course
+        client.delete("/courses/${course.id}/studygroups/${studyGroup1.id}")
+
+        delay(8000)
+
+        client.get("/courses/${course.id}/members").body<List<ScopeMember>>()
+            .apply { assertEquals(setOf(user1Id), map { it.userId }.toSet()) }
+
+        // delete second group and check members of course
+        client.delete("/courses/${course.id}/studygroups/${studyGroup2.id}")
+
+        delay(8000)
+
+        client.get("/courses/${course.id}/members").body<List<ScopeMember>>()
+            .apply {
+                assertEquals(emptySet(), map { it.userId }.toSet())
+            }
+    }
+
+    @Test
+    fun testMembersOnEnrolUnrollFromGroupsInCourse(): Unit = runBlocking {
+
+        val (studyGroup1: StudyGroupResponse, studyGroup2: StudyGroupResponse, course: CourseResponse) = startData
+
+        val user1Id = "7a98cdcf-d404-4556-96bd-4ce9137c8cbe".toUUID()
+        val user2Id = "77129e28-bf01-4dca-b19f-9fbcf576345e".toUUID()
+
+        enrolStudentsToGroups(client, studyGroup1, studyGroup2, user2Id, user1Id)
+        attachGroupsToCourse(client, course, studyGroup1, studyGroup2)
+
+        client.get("/courses/${course.id}/studygroups")
+            .body<List<String>>().apply {
+                assertEquals(map(String::toUUID).toSet(), setOf(studyGroup1.id, studyGroup2.id))
+            }
+        client.get("/courses/${course.id}/members").body<List<ScopeMember>>()
+            .apply { assertEquals(setOf(user1Id, user2Id), map { it.userId }.toSet()) }
+
+        // delete first user from first group
+        client.delete("/studygroups/${studyGroup1.id}/members/$user1Id")
+        delay(8000)
+
+        // assert only second member in first group
+        client.get("/studygroups/${studyGroup1.id}/members").body<List<ScopeMember>>()
+            .apply { assertEquals(setOf(user2Id), map { it.userId }.toSet()) }
+        // assert two members of course
+        client.get("/courses/${course.id}/members").body<List<ScopeMember>>()
+            .apply { assertEquals(setOf(user1Id, user2Id), map { it.userId }.toSet()) }
+
+        // delete second user from first group
+        client.delete("/studygroups/${studyGroup1.id}/members/$user2Id")
+        delay(8000)
+
+        // assert zero members in first group
+        client.get("/studygroups/${studyGroup1.id}/members").body<List<ScopeMember>>()
+            .apply { assertEquals(emptyList(), map { it.userId }) }
+        // assert only first member of course
+        client.get("/courses/${course.id}/members").body<List<ScopeMember>>()
+            .apply { assertEquals(listOf(user1Id), map { it.userId }) }
+
+        // delete first user from second group
+        client.delete("/studygroups/${studyGroup2.id}/members/$user1Id")
+        delay(8000)
+
+        // assert zero members of course
+        client.get("/courses/${course.id}/members").body<List<ScopeMember>>()
+            .apply { assertEquals(emptyList(), map { it.userId }) }
+    }
+
+    private suspend fun enrolStudentsToGroups(
+        client: HttpClient,
+        studyGroup1: StudyGroupResponse,
+        studyGroup2: StudyGroupResponse,
+        user2Id: UUID,
+        user1Id: UUID
+    ) {
+        client.post("/studygroups/${studyGroup1.id}/members?action=manual") {
+            contentType(ContentType.Application.Json)
+            setBody(ManualJoinMemberRequest(user1Id, roleIds = listOf(3)))
+        }.apply { assertEquals(HttpStatusCode.Created, status) }
+        client.post("/studygroups/${studyGroup2.id}/members?action=manual") {
+            contentType(ContentType.Application.Json)
+            setBody(ManualJoinMemberRequest(user1Id, roleIds = listOf(3)))
+        }.apply { assertEquals(HttpStatusCode.Created, status) }
+        client.post("/studygroups/${studyGroup1.id}/members?action=manual") {
+            contentType(ContentType.Application.Json)
+            setBody(ManualJoinMemberRequest(user2Id, roleIds = listOf(3)))
+        }.apply { assertEquals(HttpStatusCode.Created, status) }
+        delay(10000)
+    }
+
+    private suspend fun attachGroupsToCourse(
+        client: HttpClient,
+        course: CourseResponse,
+        studyGroup1: StudyGroupResponse,
+        studyGroup2: StudyGroupResponse
+    ) {
+        client.put("/courses/${course.id}/studygroups/${studyGroup1.id}")
+        client.put("/courses/${course.id}/studygroups/${studyGroup2.id}")
+        delay(10000)
+    }
+
+    @BeforeEach
+    fun initData(): Unit = runBlocking {
         val studyGroup1: StudyGroupResponse = client.post("/studygroups") {
-            authHeader()
             contentType(ContentType.Application.Json)
             setBody(CreateStudyGroupRequest("Test group 1", AcademicYear(2022, 2023)))
         }.body<StudyGroupResponse>().apply {
@@ -72,13 +196,11 @@ class CourseWithStudyGroupMembershipTest {
         }
 
         val studyGroup2: StudyGroupResponse = client.post("/studygroups") {
-            authHeader()
             contentType(ContentType.Application.Json)
             setBody(CreateStudyGroupRequest("Test group 2", AcademicYear(2022, 2025)))
         }.body()
 
         val course: CourseResponse = client.post("/courses") {
-            authHeader()
             contentType(ContentType.Application.Json)
             setBody(CreateCourseRequest("Test course 1"))
         }.run {
@@ -86,131 +208,33 @@ class CourseWithStudyGroupMembershipTest {
             assertEquals(body.name, "Test course 1")
             body
         }
-
-//        startData = StartData(studyGroup1, studyGroup2, course)
-
-        client.put("/courses/${course.id}/studygroups/${studyGroup1.id}") {
-            authHeader()
-        }
-        client.put("/courses/${course.id}/studygroups/${studyGroup2.id}") {
-            authHeader()
-        }
-
-        client.get("/courses/${course.id}/studygroups") {
-            authHeader()
-        }.body<List<String>>().apply {
-            assertEquals(map(String::toUUID).toSet(), setOf(studyGroup1.id, studyGroup2.id))
-        }
-
-        val user1Id = "7a98cdcf-d404-4556-96bd-4ce9137c8cbe".toUUID()
-        val user2Id = "77129e28-bf01-4dca-b19f-9fbcf576345e".toUUID()
-
-        client.post("/studygroups/${studyGroup1.id}/members?action=manual") {
-            authHeader()
-            contentType(ContentType.Application.Json)
-            setBody(ManualJoinMemberRequest(user1Id, roleIds = listOf(3)))
-        }.apply {
-            assertEquals(HttpStatusCode.Created, status)
-        }
-        client.post("/studygroups/${studyGroup2.id}/members?action=manual") {
-            authHeader()
-            contentType(ContentType.Application.Json)
-            setBody(ManualJoinMemberRequest(user1Id, roleIds = listOf(3)))
-        }.apply {
-            assertEquals(HttpStatusCode.Created, status)
-        }
-
-        delay(15000)
-
-        client.get("/courses/${course.id}/members") {
-            authHeader()
-            contentType(ContentType.Application.Json)
-        }.body<List<ScopeMember>>()
-            .apply {
-                assertEquals(setOf(user1Id), map { it.userId }.toSet())
-            }
-
-        client.post("/studygroups/${studyGroup1.id}/members?action=manual") {
-            authHeader()
-            contentType(ContentType.Application.Json)
-            setBody(ManualJoinMemberRequest(user2Id, roleIds = listOf(3)))
-        }.apply {
-            assertEquals(HttpStatusCode.Created, status)
-        }
-
-        delay(8000)
-
-        client.get("/courses/${course.id}/members") {
-            authHeader()
-            contentType(ContentType.Application.Json)
-        }.body<List<ScopeMember>>()
-            .apply {
-                assertEquals(setOf(user1Id, user2Id), map { it.userId }.toSet())
-            }
-
-        // delete first group and check members of course
-        client.delete("/courses/${course.id}/studygroups/${studyGroup1.id}") {
-            authHeader()
-        }
-
-        delay(8000)
-
-        client.get("/courses/${course.id}/members") {
-            authHeader()
-            contentType(ContentType.Application.Json)
-        }.body<List<ScopeMember>>()
-            .apply {
-                assertEquals(setOf(user1Id), map { it.userId }.toSet())
-            }
-
-        // delete second group and check members of course
-        client.delete("/courses/${course.id}/studygroups/${studyGroup2.id}") {
-            authHeader()
-        }
-
-        delay(8000)
-
-        client.get("/courses/${course.id}/members") {
-            authHeader()
-            contentType(ContentType.Application.Json)
-        }.body<List<ScopeMember>>()
-            .apply {
-                assertEquals(emptySet(), map { it.userId }.toSet())
-            }
-        clearData(client, studyGroup1, studyGroup2, course)
+        startData = StartData(studyGroup1, studyGroup2, course)
     }
 
-    private suspend fun clearData(
-        client: HttpClient,
-        studyGroup1: StudyGroupResponse,
-        studyGroup2: StudyGroupResponse,
-        course: CourseResponse
-    ) {
+    @AfterEach
+    fun clearData(): Unit = runBlocking {
+        val (studyGroup1: StudyGroupResponse, studyGroup2: StudyGroupResponse, course: CourseResponse) = startData
         // delete data
         assertEquals(
             HttpStatusCode.NoContent,
-            client.delete("/studygroups/${studyGroup1.id}") { authHeader() }.status
+            client.delete("/studygroups/${studyGroup1.id}").status
         )
         assertEquals(
             HttpStatusCode.NoContent,
-            client.delete("/studygroups/${studyGroup2.id}") { authHeader() }.status
+            client.delete("/studygroups/${studyGroup2.id}").status
         )
         assertEquals(
             HttpStatusCode.OK,
-            client.put("/courses/${course.id}/archived") { authHeader() }.status
+            client.put("/courses/${course.id}/archived").status
         )
         assertEquals(
             HttpStatusCode.NoContent,
-            client.delete("/courses/${course.id}") { authHeader() }.status
+            client.delete("/courses/${course.id}").status
         )
-    }
-
-    private fun HttpRequestBuilder.authHeader() {
-        header("Authorization", "Bearer $token")
     }
 }
 
-private data class StartData(
+data class StartData(
     val studyGroup1: StudyGroupResponse,
     val studyGroup2: StudyGroupResponse,
     val course: CourseResponse
