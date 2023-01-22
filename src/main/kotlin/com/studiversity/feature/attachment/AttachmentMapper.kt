849107e7d0bb308e4d10ecfc0a3398254f7af1b7
Denis <denchic150@gmail.com>
2023-01-22 22:10:00 +0300

    Update AttachmentMapper

--- src/main/kotlin/com/studiversity/feature/attachment/AttachmentMapper.kt
+++ src/main/kotlin/com/studiversity/feature/attachment/AttachmentMapper.kt
@@ -1,7 +1,9 @@
 package com.studiversity.feature.attachment
 
 import com.studiversity.database.table.AttachmentDao
+import com.studiversity.database.table.Attachments
 import com.studiversity.feature.course.element.model.*
+import org.jetbrains.exposed.sql.ResultRow
 
 fun AttachmentDao.toResponse() = when (type) {
     AttachmentType.FILE -> toFileAttachment()
@@ -13,3 +15,25 @@ fun AttachmentDao.toLinkAttachment() =
 
 fun AttachmentDao.toFileAttachment() =
     FileAttachment(id.value, FileItem(name, thumbnailUrl))
+
+fun ResultRow.toAttachment() = when (this[Attachments.type]) {
+    AttachmentType.FILE -> toFileAttachment()
+    AttachmentType.LINK -> toLinkAttachment()
+}
+
+private fun ResultRow.toLinkAttachment() = LinkAttachment(
+    this[Attachments.id].value,
+    Link(
+        url = this[Attachments.url]!!,
+        name = this[Attachments.name],
+        thumbnailUrl = this[Attachments.thumbnailUrl]
+    )
+)
+
+private fun ResultRow.toFileAttachment() = FileAttachment(
+    this[Attachments.id].value,
+    FileItem(
+        name = this[Attachments.name],
+        thumbnailUrl = this[Attachments.thumbnailUrl]
+    )
+)
