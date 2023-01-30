8362dbf4e891ca68562f313719e37bbcf97f537c
Denis <denchic150@gmail.com>
2023-01-30 22:16:55 +0300

    Create TimetableRouting.kt

--- src/main/kotlin/com/studiversity/feature/timetable/TimetableRouting.kt
+++ src/main/kotlin/com/studiversity/feature/timetable/TimetableRouting.kt
@@ -1,17 +1,16 @@
 package com.studiversity.feature.timetable
 
 import io.ktor.server.application.*
+import io.ktor.server.auth.*
 import io.ktor.server.routing.*
 
 fun Application.timetableRoutes() {
     routing {
-        route("/timetable") {
-            get {
-                val id = call.request.queryParameters["id"]
-                val weekYear: String? = call.request.queryParameters["week_year"]
-                val day: String? = call.request.queryParameters["day"]
+        authenticate("auth-jwt") {
+            route("/studygroups/{studyGroupId}/timetables/{week}") {
+                put {
 
-                //TODO get events by range date and teacher
+                }
             }
         }
     }
