b7a5839b90a12eb81db4a37d97b801908be8d47e
Denis <denchic150@gmail.com>
2023-02-02 20:01:37 +0300

    Rename to FindTimetableByStudyGroupUseCase

--- src/main/kotlin/com/studiversity/feature/timetable/TimetableDI.kt
+++ src/main/kotlin/com/studiversity/feature/timetable/TimetableDI.kt
@@ -1,12 +1,12 @@
 package com.studiversity.feature.timetable
 
-import com.studiversity.feature.timetable.usecase.GetTimetableByStudyGroupUseCase
+import com.studiversity.feature.timetable.usecase.FindTimetableByStudyGroupUseCase
 import com.studiversity.feature.timetable.usecase.PutTimetableUseCase
 import org.koin.dsl.module
 
 private val useCaseModule = module {
     single { PutTimetableUseCase(get(), get()) }
-    single { GetTimetableByStudyGroupUseCase(get(), get()) }
+    single { FindTimetableByStudyGroupUseCase(get(), get()) }
 }
 
 private val repositoryModule = module {
