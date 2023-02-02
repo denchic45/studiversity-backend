dbb9377675ab9f0f5518a23c467adfacfaf813ec
Denis <denchic150@gmail.com>
2023-02-02 20:22:28 +0300

    Improve tests with sorting

--- src/test/kotlin/com/studiversity/client/timetable/TimetableTest.kt
+++ src/test/kotlin/com/studiversity/client/timetable/TimetableTest.kt
@@ -15,6 +15,7 @@ import com.stuiversity.api.timetable.TimetableApi
 import com.stuiversity.api.timetable.model.*
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertIterableEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -22,6 +23,8 @@ import org.koin.core.parameter.parametersOf
 import org.koin.test.inject
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
+
+private const val UNKNOWN_ID = -1L
 
 class TimetableTest : KtorClientTest() {
 
@@ -41,49 +44,49 @@ class TimetableTest : KtorClientTest() {
     private lateinit var physicsCourse: CourseResponse
 
     private fun timetableForStudyGroup1() = PutTimetableRequest(
-                studyGroupId = studyGroup1.id,
-                monday = listOf(
-                    LessonRequest(1, null, listOf(teacher1Id), LessonDetails(mathCourse.id)),
-                    LessonRequest(2, null, listOf(teacher1Id), LessonDetails(mathCourse.id)),
-                    LessonRequest(3, null, listOf(teacher2Id), LessonDetails(engCourse.id))
-                ),
-                tuesday = listOf(
-                    LessonRequest(0, null, listOf(teacher1Id), LessonDetails(mathCourse.id)),
-                    LessonRequest(1, null, listOf(teacher1Id), LessonDetails(mathCourse.id)),
-                    LessonRequest(2, null, listOf(teacher2Id), LessonDetails(mathCourse.id)),
-                    LessonRequest(3, null, listOf(teacher2Id), LessonDetails(engCourse.id))
-                ),
-                wednesday = emptyList(),
-                thursday = emptyList(),
-                friday = emptyList()
-            )
+        studyGroupId = studyGroup1.id,
+        monday = listOf(
+            LessonRequest(1, null, listOf(teacher1Id), LessonDetails(mathCourse.id)),
+            LessonRequest(2, null, listOf(teacher1Id), LessonDetails(mathCourse.id)),
+            LessonRequest(3, null, listOf(teacher2Id), LessonDetails(engCourse.id))
+        ),
+        tuesday = listOf(
+            LessonRequest(0, null, listOf(teacher1Id), LessonDetails(mathCourse.id)),
+            LessonRequest(1, null, listOf(teacher1Id), LessonDetails(mathCourse.id)),
+            LessonRequest(2, null, listOf(teacher2Id), LessonDetails(mathCourse.id)),
+            LessonRequest(3, null, listOf(teacher2Id), LessonDetails(engCourse.id))
+        ),
+        wednesday = emptyList(),
+        thursday = emptyList(),
+        friday = emptyList()
+    )
 
-    fun timetableForStudyGroup2(): PutTimetableRequest = PutTimetableRequest(
-          studyGroupId = studyGroup2.id,
-          monday = listOf(
-              LessonRequest(1, null, listOf(teacher2Id), LessonDetails(mathCourse.id)),
-              LessonRequest(2, null, listOf(teacher2Id), LessonDetails(mathCourse.id)),
-              LessonRequest(3, null, listOf(teacher1Id), LessonDetails(engCourse.id))
-          ),
-          tuesday = listOf(
-              LessonRequest(0, null, listOf(teacher2Id), LessonDetails(mathCourse.id)),
-              LessonRequest(1, null, listOf(teacher2Id), LessonDetails(mathCourse.id)),
-              LessonRequest(2, null, listOf(teacher1Id), LessonDetails(mathCourse.id)),
-              LessonRequest(3, null, listOf(teacher1Id), LessonDetails(engCourse.id))
-          ),
-          wednesday = emptyList(),
-          thursday = emptyList(),
-          friday = emptyList()
-      )
+    private fun timetableForStudyGroup2(): PutTimetableRequest = PutTimetableRequest(
+        studyGroupId = studyGroup2.id,
+        monday = listOf(
+            LessonRequest(1, null, listOf(teacher2Id), LessonDetails(mathCourse.id)),
+            LessonRequest(2, null, listOf(teacher2Id), LessonDetails(engCourse.id)),
+            LessonRequest(3, null, listOf(teacher1Id), LessonDetails(mathCourse.id))
+        ),
+        tuesday = listOf(
+            LessonRequest(0, null, listOf(teacher2Id), LessonDetails(mathCourse.id)),
+            LessonRequest(1, null, listOf(teacher2Id), LessonDetails(mathCourse.id)),
+            LessonRequest(2, null, listOf(teacher1Id), LessonDetails(mathCourse.id)),
+            LessonRequest(3, null, listOf(teacher1Id), LessonDetails(engCourse.id))
+        ),
+        wednesday = emptyList(),
+        thursday = emptyList(),
+        friday = emptyList()
+    )
 
     @BeforeEach
     fun init(): Unit = runBlocking {
         studyGroup1 = studyGroupApi.create(
-            CreateStudyGroupRequest("Test group PKS 4", AcademicYear(2019, 2023), null, null)
+            CreateStudyGroupRequest("Test group PKS 4.1", AcademicYear(2019, 2023), null, null)
         ).also(::assertResultOk).unwrap()
 
         studyGroup2 = studyGroupApi.create(
-            CreateStudyGroupRequest("Test group SSA 4", AcademicYear(2023, 2027), null, null)
+            CreateStudyGroupRequest("Test group PKS 4.2", AcademicYear(2023, 2027), null, null)
         ).also(::assertResultOk).unwrap()
 
         mathCourse = courseApi.create(CreateCourseRequest("Math")).also(::assertResultOk).unwrap()
@@ -110,7 +113,10 @@ class TimetableTest : KtorClientTest() {
         val request = timetableForStudyGroup1()
         val response = putTimetable(request)
 
-        assertIterableEquals(combineAllPeriods(request).map(PeriodRequest::details), combineAllPeriods(response).map(PeriodResponse::details))
+        assertIterableEquals(
+            request.toFlatPeriods().map(PeriodRequest::details),
+            response.toFlatPeriods().map(PeriodResponse::details)
+        )
     }
 
     private suspend fun putTimetable(request: PutTimetableRequest): TimetableResponse {
@@ -125,7 +131,10 @@ class TimetableTest : KtorClientTest() {
         val response = timetableApi.getTimetableByStudyGroupId(weekOfYear, studyGroup1.id)
             .also(::assertResultOk).unwrap()
 
-        assertIterableEquals(combineAllPeriods(request).map(PeriodRequest::details), combineAllPeriods(response).map(PeriodResponse::details))
+        assertIterableEquals(
+            request.toFlatPeriods().map(PeriodRequest::details),
+            response.toFlatPeriods().map(PeriodResponse::details)
+        )
     }
 
     @Test
@@ -138,27 +147,228 @@ class TimetableTest : KtorClientTest() {
         val response = timetableApi.getTimetable(weekOfYear, memberIds = listOf(teacher1Id))
             .also(::assertResultOk).unwrap()
 
-        val expectedPeriodDetails = listOf(
-            LessonDetails(mathCourse.id),
-            LessonDetails(mathCourse.id),
-            LessonDetails(engCourse.id),
-
-            LessonDetails(mathCourse.id),
-            LessonDetails(mathCourse.id),
-            LessonDetails(mathCourse.id),
-            LessonDetails(engCourse.id)
+        val expectedTimetableResponse = TimetableResponse(
+            monday = listOf(
+                LessonResponse(
+                    UNKNOWN_ID,
+                    LocalDate.of(2023, 1, 30),
+                    1,
+                    null,
+                    studyGroup1.id,
+                    listOf(teacher1Id),
+                    LessonDetails(mathCourse.id)
+                ),
+                LessonResponse(
+                    UNKNOWN_ID,
+                    LocalDate.of(2023, 1, 30),
+                    2,
+                    null,
+                    studyGroup1.id,
+                    listOf(teacher1Id),
+                    LessonDetails(mathCourse.id)
+                ),
+                LessonResponse(
+                    UNKNOWN_ID,
+                    LocalDate.of(2023, 1, 30),
+                    3,
+                    null,
+                    studyGroup2.id,
+                    listOf(teacher1Id),
+                    LessonDetails(mathCourse.id)
+                )
+            ),
+            tuesday = listOf(
+                LessonResponse(
+                    UNKNOWN_ID,
+                    LocalDate.of(2023, 1, 31),
+                    0,
+                    null,
+                    studyGroup1.id,
+                    listOf(teacher1Id),
+                    LessonDetails(mathCourse.id)
+                ),
+                LessonResponse(
+                    UNKNOWN_ID,
+                    LocalDate.of(2023, 1, 31),
+                    1,
+                    null,
+                    studyGroup1.id,
+                    listOf(teacher1Id),
+                    LessonDetails(mathCourse.id)
+                ),
+                LessonResponse(
+                    UNKNOWN_ID,
+                    LocalDate.of(2023, 1, 31),
+                    2,
+                    null,
+                    studyGroup2.id,
+                    listOf(teacher1Id),
+                    LessonDetails(mathCourse.id)
+                ),
+                LessonResponse(
+                    UNKNOWN_ID,
+                    LocalDate.of(2023, 1, 31),
+                    3,
+                    null,
+                    studyGroup2.id,
+                    listOf(teacher1Id),
+                    LessonDetails(engCourse.id)
+                )
+            ),
+            wednesday = emptyList(),
+            thursday = emptyList(),
+            friday = emptyList(),
+            saturday = listOf()
         )
 
-
-        val periodResponseDetailsByMemberId = combineAllPeriods(response).map(PeriodResponse::details)
-        assertIterableEquals(expectedPeriodDetails, periodResponseDetailsByMemberId)
+        expectedTimetableResponse.toFlatPeriods().zip(response.toFlatPeriods())
+            .forEach { (expected, actual) ->
+                assertEquals(expected.date, actual.date)
+                assertEquals(expected.order, actual.order)
+                assertEquals(expected.studyGroupId, actual.studyGroupId)
+                assertEquals(expected.details, actual.details) { expected.toString() + actual.toString() }
+                assertEquals(expected.roomId, actual.roomId)
+            }
     }
 
-    private fun combineAllPeriods(request: PutTimetableRequest) = with(request) {
+    @Test
+    fun testGetTimetableByCourseId(): Unit = runBlocking {
+        val request1 = timetableForStudyGroup1()
+        putTimetable(request1)
+        val request2 = timetableForStudyGroup2()
+        putTimetable(request2)
+
+        val response = timetableApi.getTimetable(
+            weekOfYear = weekOfYear,
+            courseIds = listOf(mathCourse.id),
+            sorting = arrayOf(SortingPeriods.StudyGroup())
+        ).also(::assertResultOk).unwrap()
+
+        val expectedTimetableResponse = TimetableResponse(
+            monday = listOf(
+                LessonResponse(
+                    UNKNOWN_ID,
+                    LocalDate.of(2023, 1, 30),
+                    1,
+                    null,
+                    studyGroup1.id,
+                    listOf(teacher1Id),
+                    LessonDetails(mathCourse.id)
+                ),
+                LessonResponse(
+                    UNKNOWN_ID,
+                    LocalDate.of(2023, 1, 30),
+                    2,
+                    null,
+                    studyGroup1.id,
+                    listOf(teacher1Id),
+                    LessonDetails(mathCourse.id)
+                ),
+                LessonResponse(
+                    UNKNOWN_ID,
+                    LocalDate.of(2023, 1, 30),
+                    1,
+                    null,
+                    studyGroup2.id,
+                    listOf(teacher2Id),
+                    LessonDetails(mathCourse.id)
+                ),
+                LessonResponse(
+                    UNKNOWN_ID,
+                    LocalDate.of(2023, 1, 30),
+                    3,
+                    null,
+                    studyGroup2.id,
+                    listOf(teacher1Id),
+                    LessonDetails(mathCourse.id)
+                )
+            ),
+            tuesday = listOf(
+                LessonResponse(
+                    UNKNOWN_ID,
+                    LocalDate.of(2023, 1, 31),
+                    0,
+                    null,
+                    studyGroup1.id,
+                    listOf(teacher2Id),
+                    LessonDetails(mathCourse.id)
+                ),
+                LessonResponse(
+                    UNKNOWN_ID,
+                    LocalDate.of(2023, 1, 31),
+                    1,
+                    null,
+                    studyGroup1.id,
+                    listOf(teacher2Id),
+                    LessonDetails(mathCourse.id)
+                ),
+                LessonResponse(
+                    UNKNOWN_ID,
+                    LocalDate.of(2023, 1, 31),
+                    2,
+                    null,
+                    studyGroup1.id,
+                    listOf(teacher1Id),
+                    LessonDetails(mathCourse.id)
+                ),
+                LessonResponse(
+                    UNKNOWN_ID,
+                    LocalDate.of(2023, 1, 31),
+                    0,
+                    null,
+                    studyGroup2.id,
+                    listOf(teacher2Id),
+                    LessonDetails(mathCourse.id)
+                ),
+                LessonResponse(
+                    UNKNOWN_ID,
+                    LocalDate.of(2023, 1, 31),
+                    1,
+                    null,
+                    studyGroup2.id,
+                    listOf(teacher2Id),
+                    LessonDetails(mathCourse.id)
+                ),
+
+                LessonResponse(
+                    UNKNOWN_ID,
+                    LocalDate.of(2023, 1, 31),
+                    2,
+                    null,
+                    studyGroup2.id,
+                    listOf(teacher1Id),
+                    LessonDetails(mathCourse.id)
+                ),
+            ),
+            wednesday = emptyList(),
+            thursday = emptyList(),
+            friday = emptyList(),
+            saturday = emptyList()
+        )
+
+        expectedTimetableResponse.toFlatPeriods().zip(response.toFlatPeriods())
+            .forEach { (expected, actual) ->
+                assertEquals(expected.studyGroupId, actual.studyGroupId)
+                assertEquals(expected.date, actual.date)
+                assertEquals(expected.details, actual.details) { expected.toString() + actual.toString() }
+                assertEquals(expected.order, actual.order)
+                assertEquals(expected.roomId, actual.roomId)
+            }
+    }
+
+    @Test
+    fun testGetTimetableByRoom(): Unit = runBlocking {
+        val request1 = timetableForStudyGroup1()
+        putTimetable(request1)
+        val request2 = timetableForStudyGroup2()
+        putTimetable(request2)
+    }
+
+    private fun PutTimetableRequest.toFlatPeriods() = run {
         monday + tuesday + wednesday + thursday + friday + saturday
     }
 
-    private fun combineAllPeriods(response: TimetableResponse) = with(response) {
+    private fun TimetableResponse.toFlatPeriods() = run {
         monday + tuesday + wednesday + thursday + friday + saturday
     }
 }
